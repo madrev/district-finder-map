097db89d7e967197bc714d7d00209fac9722b0d2
Maddie Revill <maddierevill@gmail.com>
2017-02-22 19:57:50 -0800

    Move index to root directory; nix Twitter buttton for now

--- frontend/bundle.js
+++ frontend/bundle.js
@@ -81,13 +81,13 @@ var displayRep = exports.displayRep = function displayRep(rep) {
     $("#rep-name").html("This congressional seat is currently vacant.");
     $("#rep-details").addClass("hidden");
   } else {
+    generateTwitterButton(rep);
     $("#rep-name").text("Your rep is " + rep.first_name + " " + rep.last_name);
     $("#rep-party").html("" + rep.party);
     $("#rep-phone").html("" + rep.phone);
     $("#rep-website").html("" + rep.website);
     $("#rep-website").attr("href", "" + rep.website);
     $("#rep-details").removeClass("hidden");
-    generateTwitterButton(rep);
   }
   $("#rep-display").removeClass("hidden");
   $("#no-rep-results").addClass("hidden");
@@ -97,8 +97,9 @@ var generateTwitterButton = function generateTwitterButton(rep) {
   if (rep.twitter_id) {
     $("#rep-twitter").html("" + rep.twitter_id);
     $("#rep-twitter").attr("href", "https://www.twitter.com/" + rep.twitter_id);
-    $("#twitter-button").html(" <a href=\"https://twitter.com/" + rep.twitter_id + "\" class=\"twitter-follow-button\" data-show-count=\"false\" data-show-screen-name=\"false\">Follow</a>");
-    twttr.widgets.load();
+    // Skipping the twitter button until I can find a way to improve latency
+    // $("#twitter-button").html(` <a href="https://twitter.com/${rep.twitter_id}" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow</a>`);
+    // twttr.widgets.load($("#twitter-button").get(0));
   } else {
     $("#rep-twitter").html("Not Available");
     $("#rep-twitter").removeAttr("href");
